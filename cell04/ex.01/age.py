03845a18f7ea51b541b80fa095d2433c89e4afdb
koranan123 <koranan256@gmail.com>
2025-06-18 05:29:39 +0000

    ex.01

--- cell04/ex.01/age.py
+++ cell04/ex.01/age.py
@@ -6,5 +6,4 @@ age_in_30_years = age + 30
 
 print(f"In 10 years,you will be {age_in_10_years})
 print(f"In 20 years,you will be {age_in_20_years})
-print(f"In 30 years,you will be {age_in_30_years})
-      
+print(f"In 30 years,you will be {age_in_30_years})      
