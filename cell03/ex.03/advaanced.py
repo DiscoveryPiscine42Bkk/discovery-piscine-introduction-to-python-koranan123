78ebef789b75fb85c3b68aa8655aeba119bf63ab
koranan123 <koranan256@gmail.com>
2025-06-18 04:08:32 +0000

    ex.03

---
+++ cell03/ex.03/advaanced.py
@@ -0,0 +1,14 @@
+def print_multiplication_tables():
+    print("Multiplication Tables")
+    print("_____________________")
+
+    for i in range(1,11):
+        print(f"Table for {i}:")
+        for j in range(1,11):
+          result + i * 
+            print(f" {i} X {j} = {result}")
+           
+            print("")
+          
+          print_multiplication_tables()
+          
