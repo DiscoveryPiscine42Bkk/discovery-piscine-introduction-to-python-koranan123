c0d17e4e77806e71d0ab89820813bd389fe29d4a
koranan123 <koranan256@gmail.com>
2025-06-18 04:24:11 +0000

    ex.01

---
+++ cell04/ex.01/age.py
@@ -0,0 +1,10 @@
+age = int(input("Please enter your age: "))
+
+age_in_10_years = age + 10
+age_in_20_years = age + 20
+age_in_30_years = age + 30
+
+print(f"In 10 years,you will be {age_in_10_years})
+print(f"In 20 years,you will be {age_in_20_years})
+print(f"In 30 years,you will be {age_in_30_years})
+      
